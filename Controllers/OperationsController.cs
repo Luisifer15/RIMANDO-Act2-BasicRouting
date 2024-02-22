5d6dcde364b7a8c7fb0eb9d05b392d75414c06e0
Luis Rimando <alfonsorimando15@gmail.com>
2024-02-22 18:44:01 +0800

    Add project files.

---
+++ Controllers/OperationsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Act2___BasicRouting.Controllers
+{
+    public class OperationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Change the URL to the following format: https://www.localhost:[port]/Operations/{operation}/{num1}/{num2}.");
+        }
+        public IActionResult Add(int num1, int num2)
+        {
+            int sum = (num1 + num2);
+            return Content($"{num1} + {num2} = {sum}");
+        }
+        public IActionResult Subtract(int num1, int num2)
+        {
+            int diff = (num1 - num2);
+            return Content($"{num1} - {num2} = {diff}");
+        }
+        public IActionResult Multiply(int num1, int num2)
+        {
+            int prod = (num1 * num2);
+            return Content($"{num1} * {num2} = {prod}");
+        }
+        public IActionResult Divide(int num1, int num2)
+        {
+            float quot = ((float)num1 / (float)num2);
+            return Content($"{num1} / {num2} = {quot}");
+        }
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Act2___BasicRouting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
